8a98498a2c53a8dd71cd0ab394299837d1a182ef
Asliddin8 <kab09281@gmail.com>
2023-11-16 17:11:59 +0300

    Git hub ochdim

---
+++ src/components/Formcontrol.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <form @submit.prevent>
+            <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input v-model="message" type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+
+            <button class="btn btn-primary">Send</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            data:{
+                message:null,
+            }
+        }
+    },
+    methods:{
+        Send(){
+            this.axios.post()
+        }
+    }
+}
+</script>
